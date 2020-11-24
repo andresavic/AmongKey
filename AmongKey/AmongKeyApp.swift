e81d2d27efb5300e57fa0c07e45bda1efffe9f90
Andre Savic <andresavic@icloud.com>
2020-11-24 18:23:05 +0100

    Initial Commit

---
+++ AmongKey/AmongKeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmongKeyApp.swift
+//  AmongKey
+//
+//  Created by Andre Savic on 24.11.20.
+//
+
+import SwiftUI
+
+@main
+struct AmongKeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AmongKey/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AmongKey
+//
+//  Created by Andre Savic on 24.11.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
